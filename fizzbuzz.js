9fbed2e5a99e352a1c0d19247250d81c0e5f01d6
che.doddington <che.doddington@gmail.com>
2015-06-27 12:38:01 -0400

    Completed FizzBuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -8,10 +8,10 @@ for(i=0;i<=rangeLimit;i++){
 		console.log('FizzBuzz');
 	// If number is only divisible by 3, print Fizz	
 	} else if (i % 3 === 0) {
-			console.log ('Fizz');
+		console.log ('Fizz');
 	// If number is divisible by 5, print Buzz	
 	} else if (i % 5 === 0) {
-			console.log ('Buzz');
+		console.log ('Buzz');
 	// If number is not divisible by 3 or 5, print number	
 	} else console.log (i);
 }
